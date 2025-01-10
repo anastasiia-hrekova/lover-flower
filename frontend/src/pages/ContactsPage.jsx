97af40c4e3eaee111333c3f9845eeddb52c5d5d0
anastasiia-hrekova <anastasia.grekova1@gmail.com>
2025-01-10 17:56:26 +0200

    add not found page

--- frontend/src/pages/ContactsPage.jsx
+++ frontend/src/pages/ContactsPage.jsx
@@ -269,13 +269,11 @@ const ContactsPage = () => {
           </QuestionsBlock>
           <ContactsBlockSpan>Ми на карті</ContactsBlockSpan>
           <ContactsIframe
-            class="contacts-section__map"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2537.4757931944964!2d30.77264847656058!3d50.50671337160291!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4d97de411b701%3A0x48116b28f7f00d94!2z0YPQuy4g0KLQsNGA0LDRgdCwINCo0LXQstGH0LXQvdC60L4sIDUsINCR0YDQvtCy0LDRgNGLLCDQmtC40LXQstGB0LrQsNGPINC-0LHQu9Cw0YHRgtGMLCAwNzQwMA!5e0!3m2!1sru!2sua!4v1720375077819!5m2!1sru!2sua"
             width="100%"
             height="427"
-            allowfullscreen=""
             loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"
+            referrerPolicy="no-referrer-when-downgrade"
           ></ContactsIframe>
         </Container>
       </Contacts>
--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,8 +1,61 @@
+import { NavLink } from 'react-router-dom';
+import Footer from '../components/Footer';
+import styled from 'styled-components';
+import Container from '../styles/Container';
+
+const NotFoundPage = styled.div`
+  background-image: url(/images/404-background.png);
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  content: '';
+  width: 100%;
+  height: 95rem;
+  margin-top: 10rem;
+`;
+
+const TextBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  margin-top: 74rem;
+  margin-left: 70%;
+`;
+
+const ErrorText = styled.p`
+  font-size: 1.4rem;
+  font-weight: 400;
+  line-height: 2.1rem;
+  letter-spacing: 0.04rem;
+  font-family: 'Oswald', sans-serif;
+  text-transform: uppercase;
+`;
+
+const ToMainPage = styled(NavLink)`
+  font-size: 1.4rem;
+  font-weight: 400;
+  line-height: 2.1rem;
+  letter-spacing: 0.04rem;
+  font-family: 'Oswald', sans-serif;
+  text-transform: uppercase;
+  color: ${({ theme }) => theme.colors.mainColor};
+  text-decoration: underline;
+`;
+
 const NotFound = () => {
   return (
-    <div>
-      <h1>404</h1>
-    </div>
+    <>
+      <NotFoundPage>
+        <Container>
+          <TextBlock>
+            <ErrorText>Помилка 404</ErrorText>
+            <ErrorText>Упс...такої сторінки нема</ErrorText>
+            <ToMainPage to="/">На головну</ToMainPage>
+          </TextBlock>
+        </Container>
+      </NotFoundPage>
+      <Footer />
+    </>
   );
 };
 
