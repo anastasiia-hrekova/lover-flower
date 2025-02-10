5301c15a7de2d879736dc91d3a06ea83c17070e3
anastasiia-hrekova <anastasia.grekova1@gmail.com>
2025-02-10 19:32:54 +0200

    edit .env & api connection

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -4,4 +4,6 @@ const axiosURL = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL || 'http://localhost:4000',
 });
 
+console.log(process.env.REACT_APP_BASE_URL);
+
 export default axiosURL;
